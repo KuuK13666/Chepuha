0df7c709e1f5548540c1b8d5058d1d616b9c64bb
KuuK13666 <stepan.lesnov@mail.ru>
2025-08-10 14:25:07 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,5 +8,6 @@ from dotenv import load_dotenv
 load_dotenv()  # №2: Подтянуть переменные из .env в окружение процесса
 
 BOT_TOKEN: str | None = getenv("BOT_TOKEN")  # №3: Токен бота
-#DB_PATH: Path = Path("/app/data/bot.db")     # №4: БД хранится в томе /app/data (см. docker-compose.yml)
-DB_PATH: Path = Path("bot.db")
+DB_PATH: Path = Path("/app/data/bot.db")     # №4: БД хранится в томе /app/data (см. docker-compose.yml)
+
+#DB_PATH: Path = Path("bot.db")
